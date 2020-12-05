51951a5f033ea606744615360a72638aa269be07
gothamloop <g.otham@webtunnelo.com>
2020-12-05 10:35:26 +0100

    First Commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  //const apiRoot = 'http://localhost:8080/v1/task/';
-  const apiRoot = 'https://calm-eyrie-55061.herokuapp.com/v1/task/';
- // const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'http://localhost:8080/v1/task/';
+  //const apiRoot = 'https://calm-eyrie-55061.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
